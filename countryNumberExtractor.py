8a0e2b81c67d481b64cd58edde249185bcf194d3
Harsh negi <harshnegi6477@gmail.com>
2020-08-30 23:56:16 +0530

    slightly modified number extractor

--- countryNumberExtractor.py
+++ countryNumberExtractor.py
@@ -1,8 +1,9 @@
 import re
 
-str1 = '''This is a sample string which I am writing to store the numbers which have origin from india 
+str1 = '''
+This is a sample string which I am writing to store the numbers which have origin from india 
 +917836952729 this is my number but dont call alright? +919818678491 this adis number you can call here
-+914582348023 I dont know whose number this is but still good. 929846305738 this is a pak number
++914582348023 I dont know whose number this is but still good. +929846305738 this is a pak number
 stay alert!! Now here comes numbers from the united states of america: +1-555-748-1098 its john cena's number, +1-444-979-7071 this is randy ortons number, +1-999-100-0001 this is bill gates number, call now to contact him..
 '''
 
